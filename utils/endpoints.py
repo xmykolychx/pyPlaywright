72f8f56248cfb382616ad1b9d42bd79fac7dc0d8
xmykolychx <mykola.levchuk@teladochealth.com>
2024-03-07 08:12:53 -0500

    endpoints

---
+++ utils/endpoints.py
@@ -0,0 +1,22 @@
+class Endpoints:
+    BASE_URL: str = 'https://demoqa.com'
+
+    OPTIONS_FORMS_URL: str = f"{BASE_URL}/forms"
+    PRACTICE_FORM_URL: str = f"{BASE_URL}/automation-practice-form"
+
+    ELEMENTS_PAGE_URL: str = f"{BASE_URL}/elements"
+    BUTTONS_URL: str = f"{BASE_URL}/buttons"
+
+    LINKS_URL: str = f"{BASE_URL}/links"
+
+    DYNAMIC_PROPERTIES_URL: str = f"{BASE_URL}/dynamic-properties"
+
+    WIDGETS_URL: str = f"{BASE_URL}/widgets"
+
+    SLIDER_URL: str = f"{BASE_URL}/slider"
+
+    TABS_URL: str = f"{BASE_URL}/tabs"
+
+    PROGRESS_BAR_URL: str = f"{BASE_URL}/progress-bar"
+
+    ALERTS_WINDOWS_URL: str = f"{BASE_URL}/alertsWindows"
