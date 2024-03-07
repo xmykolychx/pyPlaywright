92674d804326f8960c6d935c1a847e038ed6e010
xmykolychx <mykola.levchuk@teladochealth.com>
2024-03-07 08:14:12 -0500

    all pages file

---
+++ utils/all_pages.py
@@ -0,0 +1,20 @@
+from pages import *
+
+
+class AllPages:
+    PAGE_CLASSES = {
+        'base': BasePage,
+        'main': MainPage,
+        'options': OptionsPage,
+        'form': FormPage,
+        'elements': ElementsPage,
+        'buttons': ButtonsPage,
+        'links': LinksPage,
+        'dynamic': DynamicPropertiesPage,
+        'widgets': WidgetsPage,
+        'slider': SliderPage,
+        'tabs': TabsPage,
+        'progress': ProgressBarPage,
+        'alerts_windows': AlertsWindowsPage,
+        'alerts': AlertsPage
+    }
