f841eb5b8b41320dc434f43241a3974aba9036b8
xmykolychx <mykola.levchuk@teladochealth.com>
2024-03-07 08:17:33 -0500

    tabs page

---
+++ pages/tabs_page.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page
+from pages.base_page import BasePage
+from utils.locators import TabsPageLocators
+from utils.endpoints import Endpoints
+
+
+class TabsPage(BasePage):
+    URL = Endpoints.TABS_URL
+
+    def __init__(self, page: Page) -> None:
+        super(TabsPage, self).__init__(page)
+
+    def click_tab(self, tab_name):
+        self.page.locator(tab_name).click()
