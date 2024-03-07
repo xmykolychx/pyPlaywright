af2ae7f9c451be2c741439c2300f3e047ad30441
xmykolychx <mykola.levchuk@teladochealth.com>
2024-03-07 08:17:42 -0500

    widgets page

---
+++ pages/widgets_page.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import Page
+from pages.base_page import BasePage
+from utils.locators import WidgetsPageLocators
+from utils.endpoints import Endpoints
+
+
+
+class WidgetsPage(BasePage):
+    URL = Endpoints.WIDGETS_URL
+
+    def __init__(self, page: Page) -> None:
+        super(WidgetsPage, self).__init__(page)
+
+    def click_slider(self):
+        self.page.locator(WidgetsPageLocators.slider_option).click()
+
+    def click_tabs(self):
+        self.page.locator(WidgetsPageLocators.tabs_option).click()
+
+    def click_progress_bar(self):
+        self.page.locator(WidgetsPageLocators.progress_bar_option).click()
